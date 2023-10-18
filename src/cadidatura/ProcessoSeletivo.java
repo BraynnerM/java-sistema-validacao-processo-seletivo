caea95062c7265502aa3c2dab814e774561f9147
Braynner Marques <braynnermarques@hotmail.com>
2023-10-18 14:43:58 -0300

    case 3 finalizado

--- src/cadidatura/ProcessoSeletivo.java
+++ src/cadidatura/ProcessoSeletivo.java
@@ -6,7 +6,20 @@ public class ProcessoSeletivo {
         // analisarCandidato(1900.0);
         // analisarCandidato(2200.0);
         // analisarCandidato(2000.0); 
-        selecaoCandidatos();       
+        selecaoCandidatos();
+        imprimirSelecionados();       
+    }
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        for (int indice = 0; indice < candidatos.length;indice++) {
+            System.out.println("O candidato de nº " + (1+indice) + " é " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
     static void selecaoCandidatos() {
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
